f8ac2a1ff4a844f942c2392f7ea72682a399de01
Denis Nuțiu <denis.nutiu@gmail.com>
2018-03-02 21:42:16 +0200

    Adding index and 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,36 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {AppComponent} from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { IndexPageComponent } from './index-page/index-page.component';
+
+const appRoutes: Routes = [
+  // { path: 'crisis-center', component: CrisisListComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: '',
+    component: IndexPageComponent,
+    data: { title: 'Novel Computation & Control' }
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     NavbarComponent,
+    PageNotFoundComponent,
+    IndexPageComponent,
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
