6e5b40a0a3f6e83f3c1527c17513e72b3ab3a7de
Denis Nuțiu <denis.nutiu@gmail.com>
2018-03-10 16:20:01 +0200

    Changing folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import {AppComponent} from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { IndexPageComponent } from './pages/index-page/index-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { AuthorCardComponent } from './author-card/author-card.component';
-import { LoginPageComponent } from './pages/authentication/login-page/login-page.component';
+import { AuthorCardComponent } from './shared/author-card/author-card.component';
+import { LoginPageComponent } from './authentication/login-page/login-page.component';
 
 
 const appRoutes: Routes = [
